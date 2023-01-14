8d5612961342b9b82697a20a5dee39d11b7272b0
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2023-01-14 00:20:29 -0300

    feat(carousel) add carousel

--- src/app/feature/tools/components/skills/skills.component.ts
+++ src/app/feature/tools/components/skills/skills.component.ts
@@ -3,9 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   template: `
-    <p>
-      skills works!
-    </p>
+  <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true" aria-label="First slide"></li>
+      <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
+      <li data-bs-target="#carouselId" data-bs-slide-to="2" aria-label="Third slide"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+      <div class="carousel-item active">
+        <img src="assets/imagens/skills/typescript.png" class="w-100 d-block" alt="First slide">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+      </button>
+  </div>
   `,
   styles: [
   ]
