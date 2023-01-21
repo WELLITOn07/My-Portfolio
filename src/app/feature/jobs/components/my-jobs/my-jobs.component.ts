7efc7864292829672f6f676caf29f07cb4e23e7e
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2023-01-21 15:52:18 -0300

    fix(restart) reset page skills and jobs

--- src/app/feature/jobs/components/my-jobs/my-jobs.component.ts
+++ src/app/feature/jobs/components/my-jobs/my-jobs.component.ts
@@ -2,27 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-jobs',
-  template: `
-    <div id="carouselIdJobs" class="carousel slide" data-bs-ride="carousel">
-    
-    <div class="carousel-inner" role="listbox">
-      <div class="carousel-item active">
-        <p>Em Construção</p>
-        <img src="assets/imagens/jobs/under-construction.png" alt="under-construction">
-      </div>
-
-    </div>
-
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselIdJobs" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselIdJobs" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-      </button>
-  </div>
-  `,
+  template: ``,
   styles: [
   ]
 })
--- src/app/feature/tools/components/skills/skills.component.ts
+++ src/app/feature/tools/components/skills/skills.component.ts
@@ -4,34 +4,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
-  template: `
-  <div id="carouselIdSkills" class="carousel slide" data-bs-ride="carousel">
-
-    <div class="carousel-inner" role="listbox">
-      <div class="carousel-item active">
-        <p>Angular</p>
-        <img src="assets/imagens/skills/angular.png" alt="Angular">
-      </div>
-        
-      <div *ngFor="let item of skillsArray">
-        <div class="carousel-item">
-          <p>{{ item.name }}</p>
-          <img src="assets/imagens/skills/{{item.path}}.png" alt="{{item.name + '.img'}}">
-        </div>
-      </div>
-
-    </div>
-
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselIdSkills" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselIdSkills" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-      </button>
-  </div>
-  `,
+  template: ``,
   styles: [
   ]
 })
